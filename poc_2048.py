3ce11e10b5b073481e7e0b17928f8c336f41f19a
Jenna Hildebrand <jennah@jennah-mac.local>
2015-05-30 15:41:59 -0700

    merge function moves blank squares (0) to right side of line

--- poc_2048.py
+++ poc_2048.py
@@ -16,12 +16,33 @@ def merge(line):
     the tile values from line slid towards the front of the list and merged.
     Note that you should return a new list and you should not modify the input list.
     """
+    #print line.count(0)
+    #print line.count(2)
+    #print line.count(4)
+    #print line
 
+    # create a result list with the same number of 0s as the length of line
+    # iterate over the line input looking for non-zero entries. For each non-zero entry,
+    # put the value into the next available entry of the result list (starting at position 0).
+    result_list = []
 
+    for number in line:
+        result_list.insert(0,0)
+        if number != 0:
+            result_list.remove(0)
+            #this won't work because it moves the order of the last numbers unless there's only one 0
+            result_list.append(number)
+
+    for number in line:
+        if number == 0:
+            result_list.remove(0)
+            result_list.append(0)
+
+    return result_list
 
 def test_merge():
     """
-    test code for merge function
+    tests for merge function
     """
 
     line1 = [2, 0, 2, 4]
@@ -31,8 +52,10 @@ def test_merge():
     line5 = [8, 16, 16, 8]
 
     # put this in a loop
-    print "Testing line1 - Computed:" merge(line1), "Expected:", [4, 4, 0, 0]
-    print "Testing line2 - Computed:" merge(line2), "Expected:", [4, 0, 0, 0]
-    print "Testing line3 - Computed:" merge(line3), "Expected:", [4, 0, 0, 0]
-    print "Testing line4 - Computed:" merge(line4), "Expected:", [4, 4, 2, 0, 0]
-    print "Testing line5 - Computed:" merge(line5), "Expected:", [8, 32, 8, 0]
+    print "Testing line1 - Computed:", merge(line1), "Expected:", [4, 4, 0, 0]
+    print "Testing line2 - Computed:", merge(line2), "Expected:", [4, 0, 0, 0]
+    print "Testing line3 - Computed:", merge(line3), "Expected:", [4, 0, 0, 0]
+    print "Testing line4 - Computed:", merge(line4), "Expected:", [4, 4, 2, 0, 0]
+    print "Testing line5 - Computed:", merge(line5), "Expected:", [8, 32, 8, 0]
+
+test_merge()
