3d740295e17113ad7251f057472bb768a678bba7
Jenna Hildebrand <jennah@jennah-mac.local>
2015-06-06 19:07:55 -0700

    implemented all but move() function for 2048

--- poc_2048.py
+++ poc_2048.py
@@ -4,10 +4,21 @@ implementation of 2048 game
 goal: merge all pairs of like values in a single row or column
 """
 
-# class Game2048:
+import poc_2048_gui
+import random
 
-    # def __init__(self):
+# Directions, DO NOT MODIFY
+UP = 1
+DOWN = 2
+LEFT = 3
+RIGHT = 4
 
+# Offsets for computing tile indices in each direction.
+# DO NOT MODIFY this dictionary.
+OFFSETS = {UP: (1, 0),
+           DOWN: (-1, 0),
+           LEFT: (0, 1),
+           RIGHT: (0, -1)}
 
 def merge(line):
     """
@@ -42,6 +53,85 @@ def merge(line):
 
     return result_list
 
+class TwentyFortyEight:
+    """
+    Class to run the game logic.
+    """
+
+    def __init__(self, grid_height, grid_width):
+        self._grid_height = grid_height
+        self._grid_width = grid_width
+        self.reset()
+
+    def reset(self):
+        """
+        Reset the game so the grid is empty except for two
+        initial tiles.
+        """
+        self._board = [[0 for dummy_col in range(self._grid_width)]
+                          for dummy_row in range(self._grid_height)]
+        self.new_tile()
+        self.new_tile()
+
+    def __str__(self):
+        """
+        Return a string representation of the grid for debugging.
+        """
+        for row in self._board:
+            return str(row)
+
+    def get_grid_height(self):
+        """
+        Get the height of the board.
+        """
+        return self._grid_height
+
+    def get_grid_width(self):
+        """
+        Get the width of the board.
+        """
+        return self._grid_width
+
+    def move(self, direction):
+        """
+        Move all tiles in the given direction and add
+        a new tile if any tiles moved.
+        """
+        self._initial_tiles = {}
+        # this might be the place to use the 'direction' param
+        # to determine how the algorithm is applied in 'merge'
+        pass
+
+    def new_tile(self):
+        """
+        Create a new tile in a randomly selected empty
+        square.  The tile should be 2 90% of the time and
+        4 10% of the time.
+        """
+        # sample = [[0, 0, 2, 4], [4, 0, 4, 0], [8, 4, 2, 0]]
+        random_tile = random.choice([2, 2, 2, 2, 2, 2, 2, 2, 2, 4])
+        random_row = (random.randint(0, self._grid_height-1))
+        random_col = (random.randint(0, self._grid_width-1))
+
+        if self.get_tile(random_row, random_col) == 0:
+            self.set_tile(random_row, random_col, random_tile)
+        # why do sometimes get only one tile?
+        # maybe the same random index is being selected twice?
+
+
+    def set_tile(self, row, col, value):
+        """
+        Set the tile at position row, col to have the given value.
+        """
+        self._board[row][col] = value
+
+    def get_tile(self, row, col):
+        """
+        Return the value of the tile at position row, col.
+        """
+        return self._board[row][col]
+
+
 
 def test_merge():
     """
@@ -66,4 +156,23 @@ def test_merge():
     print "Testing line7 - Computed:", merge(line7), "Expected:", [8, 16, 0, 0]
     # maybe also test that len(line) matches len(result_list)
 
-test_merge()
+
+def test_full():
+    """
+    tests for whole game
+    """
+    gridsize1 = 4, 4
+    new_game = TwentyFortyEight(gridsize1)
+
+    print "Testing init - Computed:", new_game, "Expected:",
+    print "Testing get_tile - Computed:", new_game.get_tile(0,3), "Expected:",
+
+
+
+# test_merge()
+# test_full()
+poc_2048_gui.run_gui(TwentyFortyEight(4, 4))
+
+
+
+
